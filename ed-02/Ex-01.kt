9483ec2234034defd4e6d422f397fbb40fb43183
João Vitor Moraes <86670059+Su66us@users.noreply.github.com>
2023-08-26 11:43:49 -0300

    Estudo dirigido

---
+++ ed-02/Ex-01.kt
@@ -0,0 +1,39 @@
+fun main(){
+
+}
+
+class ContaBancaria (val cliente: String, var saldo: Double = null, var numero: Int, val agencia: Int){
+        
+    fun depositar(operacao: Double){
+        operacao = readLine()!!.toDouble()
+        saldo += operacao
+    }
+
+    fun retirar(operacao: Double){
+        operacao = readLine()!!.toDouble()
+        if(saldo < operacao){
+            println("Saldo insuficiente")
+        } else{
+            saldo -= operacao
+        }
+    }
+
+    fun transferir(operacao: Double, numero.saldo: ContaBancaria){
+        operacao = readLine()!!.toDouble()
+        numero = readLine()!!.toInt()
+        if (saldo < operacao){
+            println("Saldo insuficiente")
+        } else{
+            saldo -= operacao
+            numero.saldo += operacao
+        }
+    }
+
+    fun imprimirExtrato(){
+        println("Extrato:")
+        println("Cliente: $cliente")
+        println("Agência: $agencia")
+        println("Número da conta: $numero")
+        println("Saldo: R$$saldo")
+    }
+}
